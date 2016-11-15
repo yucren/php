34fe9f68d3323cd1609fa0cd118f1ac710cf2bcb
user001 <539928505@qq.com>
2016-11-15 13:20:44 +0800

    ddd

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user001
+ * Date: 2016-11-15
+ * Time: 12:50
+ */
+?>
+<html>
+<head>
+   <title>my php web</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<p>hello world</p>
+</body>
+
+
+</html>
